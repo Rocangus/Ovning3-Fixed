e9da4100047ee615b1224f4f186ab3b5d7b986b9
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 14:46:59 +0100

    3.3.4Animals added to list

--- InheritanceAndPolymorphism/Program.cs
+++ InheritanceAndPolymorphism/Program.cs
@@ -7,7 +7,15 @@ namespace Ovning3.InheritanceAndPolymorphism
     {
         static void Main(string[] args)
         {
-            var Animals = new List<Animal>();
+            var animals = new List<Animal>();
+
+            animals.Add(new Swan("Bertil", 19.8, 4, 250, true));
+            animals.Add(new Dog("Lassie", 14.7, 5, 3.2));
+            animals.Add(new Hedgehog("Olle", 7.4, 2, 453));
+            animals.Add(new Flamingo("Helge", 6.8, 3, 253, DateTime.Now));
+            animals.Add(new Bird("Wilhelm", 0.1, 2, 57));
+            animals.Add(new Pelican("Rudolf", 5.7, 3, 643, 3));
+            animals.Add(new Horse("Gustav", 175.38, 6, 4));
         }
     }
 }
