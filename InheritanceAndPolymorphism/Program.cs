edceaa919160a7ad4e67699e6e5da5f1d15cfbda
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 15:46:50 +0100

    3.2.10

--- InheritanceAndPolymorphism/Program.cs
+++ InheritanceAndPolymorphism/Program.cs
@@ -16,7 +16,8 @@ namespace Ovning3.InheritanceAndPolymorphism
             animals.Add(new Flamingo("Helge", 6.8, 3, 253, DateTime.Now));
             animals.Add(new Bird("Wilhelm", 0.1, 2, 57));
             animals.Add(new Pelican("Rudolf", 5.7, 3, 643, 3));
-            animals.Add(new Horse("Gustav", 175.38, 6, 4));
+            Horse gustav = new Horse("Gustav", 175.38, 6, 4);
+            animals.Add(gustav);
 
             StringBuilder builder = new StringBuilder();
 
@@ -26,6 +27,17 @@ namespace Ovning3.InheritanceAndPolymorphism
             }
 
             Console.WriteLine(builder.ToString());
+
+            //var dogs = new List<Dog>();
+            //dogs.Add(gustav);
+            // This does not work because an instance of Horse is not an instance of Dog or a class derived from Dog.
+            // The list must take the type Animal in order for all my animals to be accepted.
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.Stats()); 
+            }
+            // The compiler ensures that the latest override is executed and that is why each derived class has its' specific override of Stats() run.
         }
     }
 }
