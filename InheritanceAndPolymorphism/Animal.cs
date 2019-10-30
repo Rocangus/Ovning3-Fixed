1222b0f04e708ab8f37bec47c3dabf607b81d2c8
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 12:07:17 +0100

    3.2.2

---
+++ InheritanceAndPolymorphism/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3.InheritanceAndPolymorphism
+{
+    class Animal
+    {
+        public string Name { get; protected set; }
+        public double Weight { get; protected set; }
+        public int Age { get; protected set; }
+    }
+}
