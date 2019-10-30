4366347cf22152ea58015c4cea37ea822502dc2a
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 16:30:30 +0100

    3.4.8

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ovning3.Polymorphism
 {
     class Program
     {
+        // Initializes some errors into a list, then prints the messages from the errors in said list.
         static void Main(string[] args)
         {
-            
+            var errors = new List<UserError>()
+            {
+                new NumericInputError(), new TextInputError(), new NumericInputError(), new TextInputError()
+            };
+
+            foreach (UserError err in errors)
+            {
+                Console.WriteLine(err.UEMessage());
+            }
         }
     }
 }
