fb13a3cf4b69ea29788ee72cfdbd137771bd1688
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 11:36:50 +0100

    3.1.4 PersonHandler operations and static class PersonHandler

--- Ovning3/PersonHandler.cs
+++ Ovning3/PersonHandler.cs
@@ -4,42 +4,105 @@ using System.Text;
 
 namespace Ovning3
 {
-    public class PersonHandler
+    // This class is static because it only changes the state of other objects, not itself.
+    public static class PersonHandler
     {
-        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+        // Creates a new person.
+        public static Person CreatePerson(int age, string fname, string lname, double height, double weight)
         {
             if (age <= 0)
             {
-                Console.WriteLine("A person's age must be greater than zero.");
+                PrintAgeError();
                 return null;
             }
-            if (String.IsNullOrEmpty(fname)) 
+            if (String.IsNullOrEmpty(fname))
             {
-                Console.WriteLine("First name must not be empty.");
+                PrintFirstNameError();
                 return null;
             }
             if (String.IsNullOrEmpty(lname))
             {
-                Console.WriteLine("Last name must not be empty.");
+                PrintLastNameError();
                 return null;
             }
             if (height <= 0)
             {
-                Console.WriteLine("Height must be greater than zero.");
+                PrintHeightError();
                 return null;
             }
             if (weight <= 0)
             {
-                Console.WriteLine("Weight must be greater than zero.");
+                PrintWeightError();
                 return null;
             }
             Person pers = new Person(age, fname, lname, height, weight);
             return pers;
-        } 
+        }
 
-        public void SetAge(Person pers, int age)
+        // Methods that print various error messages to the console follow below.
+        private static void PrintHeightError()
+        {
+            Console.WriteLine("Height must be greater than zero.");
+        }
+
+        private static void PrintLastNameError()
+        {
+            Console.WriteLine("Last name must not be empty.");
+        }
+
+        private static void PrintFirstNameError()
+        {
+            Console.WriteLine("First name must not be empty.");
+        }
+
+        private static void PrintAgeError()
+        {
+            Console.WriteLine("A person's age must be greater than zero.");
+        }
+
+        private static void PrintWeightError()
+        {
+            Console.WriteLine("Weight must be greater than zero.");
+        }
+
+        // Getters and setters as appropriate for the different properties follow below. Guard statements omitted on purpose.
+        public static int GetAge(Person pers)
+        {
+            return pers.Age;
+        }
+        public static void SetAge(Person pers, int age)
         {
             pers.Age = age;
+        }
+
+        public static string GetFname (Person pers)
+        {
+            return pers.FName;
+        }
+
+        public static string GetLname (Person pers)
+        {
+            return pers.LName;
+        }
+
+        public static double GetHeight (Person pers)
+        {
+            return pers.Height;
+        }
+
+        public static void SetHeight(Person pers, double height)
+        {
+                pers.Height = height;
+        }
+
+        public static double GetWeight(Person pers)
+        {
+            return pers.Weight;
+        }
+
+        public static void SetWeight(Person pers, double weight)
+        {
+            pers.Weight = weight;
         }
     }
 }
