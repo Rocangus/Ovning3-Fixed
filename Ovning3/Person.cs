4b35f4305a05b45f36a82469040484ad9fbbc9a6
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 10:45:49 +0100

    Fixed project and imported files from broken Ovning3

---
+++ Ovning3/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3.Ovning3
+{
+    public class Person
+    {
+        // It is not possible to directly access these private variables from program.cs.
+        private int age;
+        private string fName, lName;
+        private double height, weight;
+
+        public int Age { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+    }
+}
---
+++ Ovning3/PersonHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3
+{
+    public class PersonHandler
+    {
+
+    }
+}
