bcc4a66533404e2d73fa44ccc1b1ad60c7b3bdfc
Rocangus <hunter_sniper112@hotmail.com>
2019-10-30 16:24:02 +0100

    3.4.6

---
+++ Polymorphism/TextInputError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3.Polymorphism
+{
+    public class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a textinput in a numericonly field. This fired an error!";
+        }
+    }
+}
